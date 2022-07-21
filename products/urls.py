41e29f06ff311aab45601a52f33bc76c07b19051
eunhyung <ooooo5h91@gmail.com>
2022-07-21 09:18:13 +0900

    코드 컨벤션 반영

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
+
 from products.views import ProductView, SubCategoryView
 
 urlpatterns = [
     path('', ProductView.as_view()),
     path('/subcategories', SubCategoryView.as_view()),
-    
-    
 ]
