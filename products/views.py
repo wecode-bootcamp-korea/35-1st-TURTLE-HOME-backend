12b49f4100689e30b5a0956688d0797f3cf4aa07
eunhyung <ooooo5h91@gmail.com>
2022-07-22 07:49:23 +0900

    상품 상세화면 조회 API 구현

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.http  import JsonResponse
 from django.views import View
 
-from products.models import SubCategory
+from products.models import Product, ProductOption, SubCategory
 
 # Create your views here.
 
@@ -23,3 +23,33 @@ class SubCategoryView(View):
             })
         
         return JsonResponse({'message':result}, status=200)
+    
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        
+        try:
+            product = Product.objects.get(id=product_id)   
+            options = product.productoption_set.filter(product_id = product.id)  
+            
+            option_list = []
+            
+            for option in options:
+                option_list.append({
+                    'size' : option.size.name,   
+                    'price' : option.price
+                })
+            
+            result = {
+                'id'             : product.id,
+                'name'           : product.name,
+                'number'         : product.number,
+                'description'    : product.description,
+                'image_url'      : product.image_url,
+                'sub_category_id': product.sub_category_id,
+                'options'        : option_list,
+            }
+            
+            return JsonResponse({'message':result}, status=200)
+            
+        except Product.DoesNotExist:
+            return JsonResponse({'message':'Product does not exist.'}, status=400)
