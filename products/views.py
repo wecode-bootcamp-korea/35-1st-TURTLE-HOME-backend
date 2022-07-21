9253977f8d024a02078001d4c5e60f936a367d9a
eunhyung <ooooo5h91@gmail.com>
2022-07-21 16:43:02 +0900

    서브카테고리 조회 API 구현

--- products/views.py
+++ products/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http  import JsonResponse
+from django.views import View
+
+from products.models import SubCategory
 
 # Create your views here.
+
+class SubCategoryView(View):
+    def get(self, request, category_id):  
+        
+        subcategories = SubCategory.objects.filter(category_id=category_id)
+        result        = []
+        
+        if not subcategories:
+            return JsonResponse({'message':'Subcategory does not exist.'}, status=401)
+        
+        for subcategory in subcategories:
+            result.append({
+                'id'         : subcategory.id,
+                'name'       : subcategory.name,
+                'image_url'  : subcategory.image_url,
+                'category_id': category_id,
+            })
+        
+        return JsonResponse({'message':result}, status=200)
