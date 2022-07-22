2137722d93cabdd961e49b273a5af13e805b8e82
eunhyung <ooooo5h91@gmail.com>
2022-07-22 14:53:36 +0900

    리뷰반영 : 리스트 컴프리헨션 적용

--- products/views.py
+++ products/views.py
@@ -27,24 +27,17 @@ class ProductDetailView(View):
         try:
             product = Product.objects.get(id=product_id)   
             options = product.productoption_set.filter(product_id = product.id)  
-
-            option_list = []
-
-            for option in options:
-                option_list.append({
-                    'size' : option.size.name,   
-                    'price' : option.price
-                })
-
-            result = {
-                'id'             : product.id,
-                'name'           : product.name,
-                'number'         : product.number,
-                'description'    : product.description,
-                'image_url'      : product.image_url,
-                'sub_category_id': product.sub_category_id,
-                'options'        : option_list,
-            }
+            
+            result = { 'id'              : product.id,
+                        'name'           : product.name,
+                        'number'         : product.number,
+                        'description'    : product.description,
+                        'image_url'      : product.image_url,
+                        'sub_category_id': product.sub_category_id,
+                        'options'        : [{ 'size':option.size.name,
+                                             'price':option.price } for option in options]}
+            
+            return JsonResponse({'result':result}, status=200)
             
         except Product.DoesNotExist:
             return JsonResponse({'message':'Product does not exist.'}, status=404)
