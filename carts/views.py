7cfae89cf182f2d7c43e8325d580774d9dea81bf
성바울 <bawool@seongbaul-ui-MacBookPro.local>
2022-07-22 11:32:36 +0900

    1차 프로젝트 장바구니 담기

--- carts/views.py
+++ carts/views.py
@@ -13,16 +13,16 @@ class CartView(View):
     def post(self, request):
         try:
             data       = json.loads(request.body)
-            user       = request.user #토큰으로 확인후 인증이된 유저
-            product_id = data['product_option_id'] # 클라이언트가 요청하는 값
+            user       = request.user
+            product_id = data['product_option_id']
             quantity   = int(data['quantity'])
 
-            if quantity <= 0: #수량이 정수가 아니면 에러 반환
+            if quantity <= 0:
                 return JsonResponse({'message' : 'QUANTITY_ERROR'}, status=400)
 
             cart, is_created = Cart.objects.get_or_create(
                 user              = user,
-                product_option_id = product_id # 클라이언트가 요청한 값이 있는 변수를 option_id로 변환해서 카트에 값을 저장
+                product_option_id = product_id
             )
             cart.quantity = cart.quantity + quantity
             cart.save()
