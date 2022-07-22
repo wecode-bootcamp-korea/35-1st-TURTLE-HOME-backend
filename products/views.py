37d895b49beb014b943599957943fcfbafb3906c
eunhyung <ooooo5h91@gmail.com>
2022-07-22 12:22:59 +0900

    리뷰반영 : 리스트 컴프리헨션 적용

--- products/views.py
+++ products/views.py
@@ -9,17 +9,14 @@ class SubCategoryView(View):
     def get(self, request, category_id):  
         
         subcategories = SubCategory.objects.filter(category_id=category_id)
-        result        = []
         
         if not subcategories:
             return JsonResponse({'message':'Subcategory does not exist.'}, status=400)
         
-        for subcategory in subcategories:
-            result.append({
-                'id'         : subcategory.id,
-                'name'       : subcategory.name,
-                'image_url'  : subcategory.image_url,
-                'category_id': category_id,
-            })
+        result = [{ 
+                    'id'         : subcategory.id,
+                    'name'       : subcategory.name,
+                    'image_url'  : subcategory.image_url,
+                    'category_id': category_id } for subcategory in subcategories]
         
-        return JsonResponse({'result':result}, status=200)
+        return JsonResponse({'result':result}, status=200)    
