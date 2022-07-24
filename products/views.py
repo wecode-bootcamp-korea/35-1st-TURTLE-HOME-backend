897a39291dd37be4753364b4d22844b5f6316b39
eunhyung <ooooo5h91@gmail.com>
2022-07-24 11:13:28 +0900

    코드 중복 줄여봄

--- products/views.py
+++ products/views.py
@@ -62,34 +62,18 @@ class ProductListView(View):
         
         if sort_condition == 'price':
                     
-            sorted_result = sorted(result, key = lambda x : x['prices'][0])
-        
-            return JsonResponse({'message':'Ordered by lowest price', 'result':sorted_result}, status=200)
+            result = sorted(result, key = lambda x : x['prices'][0])
         
         elif sort_condition == '-price':
         
-            sorted_result = sorted(result, key = lambda x : x['prices'][0], reverse=True)
-        
-            return JsonResponse({'message':'Ordered by highest price', 'result':sorted_result}, status=200)
-        
+            result = sorted(result, key = lambda x : x['prices'][0], reverse=True)
+    
         elif sort_condition == '-id':
             
-            # created_at이라는 값을 사용해서 정렬하자니 코드의 중복?이 생기는 데 이 코드도 줄이고 싶다.
-            # products = products.order_by('-created_at')
-            
-            # sorted_result = [{  'id'       : product.id, 
-            #                     'name'     : product.name,
-            #                     'image_url': product.image_url,
-            #                     'prices'   : 
-            #                         [int(p.price) for p in product.productoption_set.filter(product_id = product.id)]
-            #                     } for product in products] 
-            
-            # id도 생성한 순서대로 생기는 고유값이니까 이를 이용해서 역순으로 정렬하면 최신순이 된다!.
-            sorted_result = sorted(result, key = lambda x : x['id'], reverse=True)
-            
-            return JsonResponse({'message':'Ordered by newest', 'result':sorted_result}, status=200)
+            result = sorted(result, key = lambda x : x['id'], reverse=True)
         
         return JsonResponse({'result':result}, status=200)
+        
     
     
      
