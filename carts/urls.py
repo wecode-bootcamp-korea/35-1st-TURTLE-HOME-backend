42cf4f52303adeda2de4af2496d5b33da41c7a5e
성바울 <bawool@seongbaul-ui-MacBookPro.local>
2022-07-27 17:27:17 +0900

    회원탈퇴 API구현(url수정)

--- carts/urls.py
+++ carts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from carts.views import CartView
 
 urlpatterns = [
-    path("/cart", CartView.as_view()),
-    path("/cart<int:cart_id>", CartView.as_view())
+    path("", CartView.as_view()),
+    path("/<int:cart_id>", CartView.as_view())
 ]
