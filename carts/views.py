7b67766567ece719f742f05c115eaf7b115840b9
성바울 <bawool@seongbaul-ui-MacBookPro.local>
2022-07-27 16:14:03 +0900

    장바구니담기 기능구현(수정)

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,11 @@
 import json
 
-from django.views     import View
-from django.http      import JsonResponse
+from django.views import View
+from django.http  import JsonResponse
 
-from .models          import Cart
-from core.utils       import signin_decorator
+from .models         import Cart
+from core.utils      import signin_decorator
+from products.models import ProductOption
 
 # Create your views here.
 
@@ -14,20 +15,28 @@ class CartView(View):
         try:
             data              = json.loads(request.body)
             user              = request.user
-            product_option_id = data['product_option_id']
+            product_id        = data['product_id']
+            size_id           = data['size_id']
             quantity          = int(data['quantity'])
 
             if quantity <= 0:
                 return JsonResponse({'message' : 'QUANTITY_ERROR'}, status=400)
 
+            if not ProductOption.objects.filter(product_id=product_id, size_id=size_id).exists():
+                return JsonResponse({'message' : 'PRODUCT_DOES_NOT_EXIST'}, status=404)
+
+            product_option = ProductOption.objects.get(product_id=product_id, size_id=size_id)
+            
             cart, is_created = Cart.objects.get_or_create(
-                user              = user,
-                product_option_id = product_option_id
+                user           = user,
+                product_option = product_option
             )
             cart.quantity = cart.quantity + quantity
             cart.save()
+
+            status = 201 if is_created else 200
             
-            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+            return JsonResponse({'message' : 'SUCCESS'}, status=status)
         
         except Cart.DoesNotExist:
             return JsonResponse({'message' : 'INVALID_CART'}, status=400)
